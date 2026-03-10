c0bc5c774d5eaace0f9abcec29b4fa5a9656f562
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 21:44:36 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,17 @@
-import { SiGithub } from '@icons-pack/react-simple-icons'
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
+import { SiGithub } from '@icons-pack/react-simple-icons'
 
 const Header = () => {
   return (
-    <header className="fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]">
-      <div className="mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8">
-        
-        <Link href="/" aria-label="Home" className="flex items-center">
-          <Image
-            src="/logo.png"
-            alt="Bravia Logo"
-            height={40}
-            className="h-auto w-auto"
-            priority
-          />
+    <header className='fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]'>
+      <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
+        <Link href='/' aria-label='Home' className='flex items-center'>
+          <Image src='/logo.png' alt='Bravia Logo' height={40} className='size-auto' priority />
         </Link>
-
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/braviadev/tools"
-          aria-label="GitHub"
-        >
+        <a target='_blank' rel='noopener noreferrer' href='https://github.com/braviadev/tools' aria-label='GitHub'>
           <SiGithub />
         </a>
-
       </div>
     </header>
   )
