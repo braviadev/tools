85e259b337c65cd3e153a3349f9f01ad01ecb0f0
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 12:12:43 +0100

    Update logo.tsx

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,11 +1,1 @@
-type LogoProps = React.SVGAttributes<SVGElement>
 
-const Logo = (props: LogoProps) => {
-  return (
-    <svg version='1.0' xmlns='http://www.w3.org/2000/svg' width={640} height={715} viewBox='0 0 640 715' {...props}>
-      
-    </svg>
-  )
-}
-
-export { Logo }
