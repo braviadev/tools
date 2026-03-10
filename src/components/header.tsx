f8a3e31fadd37b35439a0d3f2b41c48f865f84c8
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 21:53:34 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,9 +7,20 @@ const Header = () => {
     <header className='fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]'>
       <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
         <Link href='/' aria-label='Home' className='flex items-center'>
-          <Image src='/logo.png' alt='Bravia Logo' height={40} className='size-auto' priority />
+          <Image
+            src='/logo.png'
+            alt='Bravia Logo'
+            height={40}
+            className='size-auto'
+            priority
+          />
         </Link>
-        <a target='_blank' rel='noopener noreferrer' href='https://github.com/braviadev/tools' aria-label='GitHub'>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/braviadev/tools'
+          aria-label='GitHub'
+        >
           <SiGithub />
         </a>
       </div>
