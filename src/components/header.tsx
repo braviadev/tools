63f8128fa25bd064e5c4a4c75666c7478747f0d9
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 21:41:49 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,31 @@
 import { SiGithub } from '@icons-pack/react-simple-icons'
 import Link from 'next/link'
-
-import { Logo } from './ui/logo'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header className='fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]'>
-      <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
-        <Link href='/' aria-label='Home'>
-          <Logo width={150} height={150} />
+    <header className="fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]">
+      <div className="mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8">
+        
+        <Link href="/" aria-label="Home" className="flex items-center">
+          <Image
+            src="/logo.png"
+            alt="Bravia Logo"
+            height={40}
+            className="h-auto w-auto"
+            priority
+          />
         </Link>
-        <a target='_blank' rel='noopener noreferrer' href='https://github.com/braviadev/tools' aria-label='GitHub'>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/braviadev/tools"
+          aria-label="GitHub"
+        >
           <SiGithub />
         </a>
+
       </div>
     </header>
   )
