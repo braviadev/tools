c89e844fa43404a405b657485de406ddfb47e056
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 20:37:51 +0100

    Update logo.tsx

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,76 +1,76 @@
-type LogoProps = React.SVGProps<SVGSVGElement>
-
-const Logo = (props: LogoProps) => {
-  return (
-    <svg viewBox='0 0 167 168' xmlns='http://www.w3.org/2000/svg' {...props}>
-      <g filter='url(#filter0)'>
-        <rect x='4' width='159' height='160' fill='black' />
-      </g>
-
-      <g filter='url(#filter1)'>
-        <path
-          d='M106 100.5C106 105.375 105.208 109.625 103.625 113.25C102.083 116.875 99.9167 119.896 97.125 122.312C94.3333 124.729 91 126.521 87.125 127.688C83.2917 128.896 79.0833 129.5 74.5 129.5H72.375C71.9583 129.5 71.3333 129.479 70.5 129.438C69.6667 129.438 68.8125 129.354 67.9375 129.188C67.1042 129.062 66.3542 128.833 65.6875 128.5C65.0625 128.167 64.75 127.708 64.75 127.125C64.75 126.458 64.8958 125.792 65.1875 125.125C65.5208 124.5 65.9375 123.938 66.4375 123.438C66.9375 122.896 67.5 122.458 68.125 122.125C68.75 121.75 69.375 121.5 70 121.375C69.6667 120.958 69.4167 120.396 69.25 119.688C69.0833 118.979 68.9375 118.229 68.8125 117.438C68.6875 116.646 68.5833 115.875 68.5 115.125C68.4583 114.333 68.4167 113.667 68.375 113.125C67.625 106.667 66.9792 100.229 66.4375 93.8125C65.8958 87.3958 65.625 80.9167 65.625 74.375C65.625 70.2083 65.6875 66.0833 65.8125 62C65.9375 57.9167 66.0417 53.7917 66.125 49.625C66.2083 47.9167 66.2292 46.2292 66.1875 44.5625C66.1875 42.8958 66.25 41.2083 66.375 39.5Z'
-          fill='white'
+import * as React from "react"
+import { SVGProps } from "react"
+const Logo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={167}
+    height={168}
+    fill="none"
+    {...props}
+  >
+    <g filter="url(#a)">
+      <path fill="#000" d="M4 0h159v160H4z" />
+    </g>
+    <g filter="url(#b)">
+      <path
+        fill="#fff"
+        d="M106 100.5c0 4.875-.792 9.125-2.375 12.75-1.542 3.625-3.708 6.646-6.5 9.062-2.792 2.417-6.125 4.209-10 5.376-3.833 1.208-8.042 1.812-12.625 1.812h-2.125c-.417 0-1.042-.021-1.875-.062-.833 0-1.688-.084-2.563-.25a7.52 7.52 0 0 1-2.25-.688c-.624-.333-.937-.792-.937-1.375 0-.667.146-1.333.438-2a7.092 7.092 0 0 1 1.25-1.687 6.35 6.35 0 0 1 1.687-1.313 5.893 5.893 0 0 1 1.875-.75c-.333-.417-.583-.979-.75-1.687a30.943 30.943 0 0 1-.438-2.25 50.858 50.858 0 0 1-.312-2.313 85.088 85.088 0 0 0-.125-2 618.277 618.277 0 0 1-1.938-19.313 230.97 230.97 0 0 1-.812-19.437c0-4.167.063-8.292.188-12.375.124-4.083.229-8.208.312-12.375.083-1.708.104-3.396.063-5.063 0-1.666.062-3.354.187-5.062-.25.083-.52.125-.813.125h-.937a5.9 5.9 0 0 1-1.875-.313c-.583-.25-.875-.812-.875-1.687 0-1.083.23-1.958.688-2.625a4.336 4.336 0 0 1 1.75-1.5 6.298 6.298 0 0 1 2.374-.688 19.882 19.882 0 0 1 2.688-.187c4 0 7.875.5 11.625 1.5s7.063 2.542 9.938 4.625c2.874 2.083 5.187 4.75 6.937 8 1.75 3.208 2.625 7.042 2.625 11.5 0 3.667-.667 7.167-2 10.5s-3.292 6.292-5.875 8.875c2.333 1 4.333 2.313 6 3.938a20.795 20.795 0 0 1 4.187 5.374c1.084 2 1.876 4.167 2.376 6.5.541 2.292.812 4.646.812 7.063ZM91 57.375c0-2.5-.396-4.708-1.188-6.625-.791-1.958-1.895-3.646-3.312-5.063-1.417-1.458-3.104-2.624-5.063-3.5-1.958-.916-4.104-1.604-6.437-2.062.083.208.125.396.125.563v.437c0 3.958-.042 7.875-.125 11.75a535.112 535.112 0 0 0-.125 11.625V71c0 .833.02 1.688.063 2.563.041.833.062 1.687.062 2.562 1.083-.458 2.167-.688 3.25-.688a91.107 91.107 0 0 1 3.375-.062c3.125-1.958 5.458-4.52 7-7.688C90.208 64.48 91 61.043 91 57.376Zm5.625 43c0-2-.188-3.938-.563-5.813-.333-1.874-.937-3.583-1.812-5.124a13.437 13.437 0 0 0-3.438-4.063c-1.416-1.125-3.187-1.958-5.312-2.5A31.352 31.352 0 0 1 75.375 86.5c.333 4.708.73 9.417 1.188 14.125.5 4.667 1.187 9.333 2.062 14a90.17 90.17 0 0 0 .688 3.25 16.56 16.56 0 0 1 .562 3.25c2.792-.375 5.23-1.146 7.313-2.313 2.124-1.208 3.874-2.708 5.25-4.5 1.416-1.833 2.458-3.937 3.124-6.312.709-2.375 1.063-4.917 1.063-7.625Z"
+      />
+    </g>
+    <defs>
+      <filter
+        id="a"
+        width={167}
+        height={168}
+        x={0}
+        y={0}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
         />
-      </g>
-
-      <defs>
-        <filter
-          id='filter0'
-          x='0'
-          y='0'
-          width='167'
-          height='168'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
-          />
-          <feOffset dy='4' />
-          <feGaussianBlur stdDeviation='2' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
-          />
-          <feBlend in2='BackgroundImageFix' result='effect1_dropShadow' />
-          <feBlend in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
-        </filter>
-
-        <filter
-          id='filter1'
-          x='57.875'
-          y='32.625'
-          width='52.125'
-          height='104.875'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
-          />
-          <feOffset dy='4' />
-          <feGaussianBlur stdDeviation='2' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
-          />
-          <feBlend in2='BackgroundImageFix' result='effect1_dropShadow' />
-          <feBlend in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
-        </filter>
-      </defs>
-    </svg>
-  )
-}
-
-export { Logo }
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2_2" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_2_2"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="b"
+        width={52.125}
+        height={104.875}
+        x={57.875}
+        y={32.625}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2_2" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_2_2"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+)
+export default Logo
