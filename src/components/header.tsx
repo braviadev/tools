ee293936dde3e6b9db59d5564dae5ceb45b0ace4
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-10 12:54:31 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,9 @@ const Header = () => {
   return (
     <header className='fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]'>
       <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
-
+        <Link href='/' aria-label='Home'>
+          <Logo width={24} height={24} />
+        </Link>
         <a target='_blank' rel='noopener noreferrer' href='https://github.com/braviadev/tools' aria-label='GitHub'>
           <SiGithub />
         </a>
